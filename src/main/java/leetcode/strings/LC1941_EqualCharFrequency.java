9c6ec85322a1c49be6e3da8755246f929a986c9d
Alok130 <alok.47489@gmail.com>
2025-05-16 11:41:51 +0530

    Add solution for Leetcode 1941: Equal Character Frequency

---
+++ src/main/java/leetcode/strings/LC1941_EqualCharFrequency.java
@@ -0,0 +1,80 @@
+package leetcode.strings;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Vector;
+
+public class LC1941_EqualCharFrequency {
+
+
+    public static void main(String[] args) {
+        String s1 = "juczjzjlsfkmpv";// Output : false
+        String s2 = "aabb";// Output : true
+        String s3 = "abacbc";// Output : true
+        String s4 = "mmmccmcccccmcccccmmmcmccmmccccmmmcmmcmcmcmcmmmmmmmmmcccmmcmmmcmmcmcmcmmmcmmmcmmccccmcmccmmcmccmmmcmmccccmcmmccmcmmcccmmcmmcmmcmccmmccmcmmcmmccmmccmcccmmcccmmcccccmcmmmmcmccmmmmmmcmmccmccmmcccccccccmcccmmmccmmccccmmcmcmcmcmmcmmcmcmcmccccmmcccmmmccmmcmmmcmmmcmccccmcmcccmmccmm";// Output : true
+        String s5 = "aa";
+
+       /* System.out.println(areOccurrencesEqual(s1));
+        System.out.println(areOccurrencesEqual(s2));
+        System.out.println(areOccurrencesEqual(s3));
+        System.out.println(areOccurrencesEqual(s4));*/
+
+        System.out.println(areOccurrencesEqualOptimized(s5));
+    }
+
+    public static boolean areOccurrencesEqual(String s) {
+
+        Vector<Integer> vectorCounter = new Vector<>();
+
+        while(!s.isEmpty()){
+            int occCounter = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(0) == s.charAt(j)){
+                    occCounter++;
+                }
+            }
+            System.out.println("Counter Value :"+occCounter);
+
+            vectorCounter.add(occCounter);
+
+            s = s.replaceAll(String.valueOf(s.charAt(0)), "");
+            System.out.println("Value : "+s);
+        }
+        // check if the array contains equal elements or not
+        // if yes return "true"
+        // else return "no"
+
+        for (int i = 0; i < vectorCounter.size()-1; i++) {
+            Integer i1 = vectorCounter.elementAt(i);
+            Integer i2 = vectorCounter.elementAt(i+1);
+            if (!i1.equals(i2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areOccurrencesEqualOptimized(String s) {
+
+        int [] frequency = new int[26];
+
+        for(char alphabet : s.toCharArray()){
+            frequency[alphabet - 'a']++;// frequency[alphabet - 'a'] or freq[index] = freq[index] + 1;
+        }
+
+        int commonFreq = 0;
+
+        for(int freq : frequency){
+            if(freq == 0){
+                continue;
+            }
+            if(commonFreq == 0){
+                commonFreq = freq;
+            } else if (freq != commonFreq) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
